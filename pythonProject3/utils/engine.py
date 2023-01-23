9149a6f6094db620b59d481a721c3481053787ad
Ovannes Shaumyan <ovannes_shaumyan@senla.eu>
2023-01-23 16:54:11 +0300

    PROJCROSS-699 add some comments

--- pythonProject3/utils/engine.py
+++ pythonProject3/utils/engine.py
@@ -344,16 +344,17 @@ def RGB_to_HEX(rgb):
 def order_points(pts):
     pts = pts.reshape(4, 2)
     rect = np.zeros((4, 2), dtype="int32")
-    # the top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
+    # у верхней левой точки будет наименьшая сумма
+    # у нижней правой точки будет наибольшая сумма
     s = pts.sum(axis=1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
-    # now, compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
+
+    # высчитываем y-х для каждой точки.
+    # у верхней правой точки результат будет наименьшим
+    # у нижней левой точки результат будет наибольшим
     diff = np.diff(pts, axis=1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
-    # return the ordered coordinates
+
     return rect
